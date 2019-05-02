c9b35a9e93ac5d5f8862d6a003a3e012bed114e7
learn <daragan1991@live.com>
2019-05-01 20:43:36 -0700

    added users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def new 
+        @user = User.new 
+    end 
+
+    def show
+        return redirect_to :root unless logged_in?
+    end 
+
+    
 end
